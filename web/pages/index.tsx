8885306e78003db2a6ff4ba1bff02b994f6f6001
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2022-11-04 01:46:08 -0300

    aula 01

---
+++ web/pages/index.tsx
@@ -0,0 +1,17 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return <div>hello {props.count}</div>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+
+  const data = await response.json();
+
+  return {
+    props: { count: data.count },
+  };
+};
